0aa4a3152b88c71f9e887ab74f6074a2d9fb8803
RogerJackD <rogerzapata114@gmail.com>
2025-12-22 23:50:46 -0500

    feat: add function datafetch users

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,71 @@
+'use client'
+
+import { Command, CommandEmpty, CommandGroup, CommandInput, CommandItem, CommandList, CommandSeparator, CommandShortcut } from "@/components/ui/command";
+import { Calculator, Calendar, CreditCard, Settings, Smile } from "lucide-react";
+import { useEffect, useState } from "react";
+import { User } from "../types/user";
+import { usersData } from "../data/user";
 
 export default function Home() {
+
+
+  const [users, setUsers] = useState<User[]>([])
+
+  useEffect(() => {
+
+    const fetchUsers = () => {
+      setUsers(()=>usersData)
+    };
+
+    fetchUsers();
+
+    console.log(users)
+  }, [users])
+  
+  
+
+
+
   return (
     <div>
       <main>
-
+      <Command className="rounded-lg border shadow-md md:min-w-[450px]">
+      <CommandInput placeholder="Type a command or search..." />
+      <CommandList>
+        <CommandEmpty>No results found.</CommandEmpty>
+        <CommandGroup heading="Suggestions">
+          <CommandItem>
+            <Calendar />
+            <span>Calendar</span>
+          </CommandItem>
+          <CommandItem>
+            <Smile />
+            <span>Search Emoji</span>
+          </CommandItem>
+          <CommandItem disabled>
+            <Calculator />
+            <span>Calculator</span>
+          </CommandItem>
+        </CommandGroup>
+        <CommandSeparator />
+        <CommandGroup heading="Settings">
+          <CommandItem>
+            <span>Profile</span>
+            <CommandShortcut>⌘P</CommandShortcut>
+          </CommandItem>
+          <CommandItem>
+            <CreditCard />
+            <span>Billing</span>
+            <CommandShortcut>⌘B</CommandShortcut>
+          </CommandItem>
+          <CommandItem>
+            <Settings />
+            <span>Settings</span>
+            <CommandShortcut>⌘S</CommandShortcut>
+          </CommandItem>
+        </CommandGroup>
+      </CommandList>
+    </Command>
       </main>
     </div>
   );
